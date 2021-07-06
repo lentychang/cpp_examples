3f9b59952594bf219af43701c8d03fba937106c5
chihpeng.chang <chih-peng.chang@directpark.de>
2021-07-06 16:02:20 +0200

    update arg_forwarding

--- arg_forwarding.cpp
+++ arg_forwarding.cpp
@@ -7,39 +7,48 @@
 
 class Logger
 {
-    std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> rot;
-    std::shared_ptr<spdlog::sinks::dup_filter_sink_mt> dup_rot;
-    std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> console;
-    std::shared_ptr<spdlog::sinks::dup_filter_sink_mt> dup_console;
-    std::shared_ptr<spdlog::sinks::dist_sink_mt> dist_sink;
-    std::shared_ptr<spdlog::logger> loggerPtr;
+    std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> m_rot;
+    std::shared_ptr<spdlog::sinks::dup_filter_sink_mt> m_dup_rot;
+    std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> m_console;
+    std::shared_ptr<spdlog::sinks::dup_filter_sink_mt> m_dup_console;
+    std::shared_ptr<spdlog::sinks::dist_sink_mt> m_dist_sink;
+    std::shared_ptr<spdlog::logger> m_loggerPtr;
 
 public:
-    Logger()
-    {
-        rot = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("log_filename", 1024 * 1024, 5, false);
-        dup_rot = std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(5));
-        dup_rot->add_sink(rot);
-        console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        dup_console = std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(5));
-        dup_console->add_sink(console);
-
-        dist_sink = std::make_shared<spdlog::sinks::dist_sink_mt>();
-        dist_sink->add_sink(dup_rot);
-        dist_sink->add_sink(dup_console);
-        loggerPtr = std::make_shared<spdlog::logger>("loggername", dist_sink);
-    }
-    inline std::shared_ptr<spdlog::logger> get_loggerPtr() { return loggerPtr; };
+    Logger(const std::string &loggername, const std::string &fname);
+    template <typename... Args>
+    void info(Args &&...args);
+    inline std::shared_ptr<spdlog::logger> get_loggerPtr() { return m_loggerPtr; };
 };
+
+Logger::Logger(const std::string &loggername, const std::string &fname)
+{
+    m_rot = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(fname, 1024 * 1024, 5, false);
+    m_dup_rot = std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(5));
+    m_dup_rot->add_sink(m_rot);
+    m_console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    m_dup_console = std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(5));
+    m_dup_console->add_sink(m_console);
+
+    m_dist_sink = std::make_shared<spdlog::sinks::dist_sink_mt>();
+    m_dist_sink->add_sink(m_dup_rot);
+    m_dist_sink->add_sink(m_dup_console);
+    m_loggerPtr = std::make_shared<spdlog::logger>(loggername, m_dist_sink);
+}
+template <typename... Args>
+void Logger::info(Args &&...args)
+{
+    m_loggerPtr->info(std::forward<Args>(args)...);
+}
 
 class Wrapper
 {
 
 public:
     static std::shared_ptr<spdlog::logger> spdlogptr;
-    static void Init();
+    static std::shared_ptr<Logger> logptr;
+    static void Init(const std::string &loggername, const std::string &fname);
 
-    static std::unordered_map<const char *, std::shared_ptr<spdlog::sinks::base_sink<std::mutex>>> sinks;
     template <typename... Args>
     static void info(Args &&...args)
     {
@@ -47,17 +56,18 @@ public:
     }
 };
 
+std::shared_ptr<Logger> Wrapper::logptr;
 std::shared_ptr<spdlog::logger> Wrapper::spdlogptr;
-void Wrapper::Init()
+void Wrapper::Init(const std::string &loggername, const std::string &fname)
 {
-
-    Wrapper::spdlogptr = std::make_shared<Logger>()->get_loggerPtr();
+    Wrapper::logptr = std::make_shared<Logger>(loggername, fname);
+    Wrapper::spdlogptr = Wrapper::logptr->get_loggerPtr();
 }
-
 int main()
 {
-    Wrapper::Init();
+    Wrapper::Init("toplogger", "flog.log");
     Wrapper::spdlogptr->info("sss{}", 1);
+    Wrapper::logptr->info("HI{}\n", 15);
     Wrapper::info("test{}", 1);
     return 0;
 }
