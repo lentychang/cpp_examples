f1dbd892006519b5112563b8f08a7175b92d0c52
chihpeng chang <chih-peng.chang@directpark.de>
2021-07-13 23:54:52 +0200

    add sqlite3 name parameter

--- sqlite3_simple.cpp
+++ sqlite3_simple.cpp
@@ -28,11 +28,23 @@ int main()
   char **errmsg;
   sqlite3_exec(db, "select * from test", callback, nullptr, errmsg);
 
-  sqlite3_stmt *stmt;
-  int res = sqlite3_prepare_v2(db, "insert into test values(?,?)", -1, &stmt, nullptr);
-  sqlite3_bind_int(stmt, 1, 10);
+  sqlite3_stmt *stmt; // :AAAA name_parameter
+
+  int res = sqlite3_prepare_v2(db, "insert into test values(?1,?2)", -1, &stmt, nullptr);
+  std::cout << "?1 index: " << sqlite3_bind_parameter_index(stmt, "?1") << std::endl;
+  std::cout << "?2 index: " << sqlite3_bind_parameter_index(stmt, "?2") << std::endl;
+
+  sqlite3_bind_int(stmt, sqlite3_bind_parameter_index(stmt, "?1"), 10);
   //stmt,pos,str,size of string, funcptr
   sqlite3_bind_text(stmt, 2, "testabc", 8, nullptr);
+  sqlite3_step(stmt);
+  sqlite3_finalize(stmt);
+  sqlite3_exec(db, "select * from test", callback, nullptr, errmsg);
+
+  res = sqlite3_prepare_v2(db, "insert into test values(:inta,:text)", -1, &stmt, nullptr);
+  sqlite3_bind_int(stmt, sqlite3_bind_parameter_index(stmt, ":inta"), 10);
+  sqlite3_bind_text(stmt, sqlite3_bind_parameter_index(stmt, ":text"), "abcde", 5, nullptr);
+  //stmt,pos,str,size of string, funcptr
   sqlite3_step(stmt);
   sqlite3_finalize(stmt);
   sqlite3_exec(db, "select * from test", callback, nullptr, errmsg);
